4f6928cc74f869ae87f3e1ef2acac9e0b559c9ae
Sudheesh A S <66734222+sudhi48@users.noreply.github.com>
2024-04-12 16:43:57 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,27 @@
-import express from 'express'
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+
+const db=mongoose.connection;
+
+db.on('connected',()=>{
+    console.log('connected to database')
+})
+
+db.on('error',(error)=>{
+    console.log(error)
+})
+
+db.on('disconnected',()=>{
+    console.log('disconnected from database')
+})
+
 
 const app=express()
+
 
 app.listen(3000, ()=>{
     console.log('Server listening to port 3000')

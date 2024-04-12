f849faa7c803b4c3ecbec2e6524981d673ae2d8f
Sudheesh A S <66734222+sudhi48@users.noreply.github.com>
2024-04-12 17:32:03 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import './db.js';
-
+import userRoutes from './routes/user.route.js';
 
 const app=express()
-
 
 app.listen(3000, ()=>{
     console.log('Server listening to port 3000')
 })
+
+app.use('/api/user',userRoutes)

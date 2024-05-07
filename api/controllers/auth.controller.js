cfb43a0f2115803c334ab7f2ade1953f4272fc55
Sudheesh A S <66734222+sudhi48@users.noreply.github.com>
2024-05-07 10:33:44 +0530

    complete sign-up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,7 +4,8 @@ import User from "../models/user.model.js"
 export const signup =async (req,res,next) => {
     try{
         const {username, email,password} = req.body
-        const newUser= new User({username,email,password})
+        const hashedPassword= bcryptjs.hashSync(user.password, 10)
+        const newUser= new User({username,email,password:hashedPassword})
         await newUser.save()
         console.log('user created successfully')
         res.status(201).json({message: 'User created successfully'})
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,3 @@
-import bcryptjs from 'bcryptjs';
 import mongoose from "mongoose";
 
 const userSchema=new mongoose.Schema({
@@ -18,22 +17,6 @@ const userSchema=new mongoose.Schema({
     },
 }, {timestamps: true});
 
-userSchema.pre('save', async function(next){
-    const user = this;
-    //hash the password only if it  has been modified (or it  is new)
-    if(!user.isModified('password')) return next();
-    try{
-
-        //hash password
-        const hashedPassword= bcryptjs.hashSync(user.password, 10);
-        //override the plain password with hashed one
-        user.password=hashedPassword;
-        next();
-    }
-    catch(err){
-        return next(err);
-    }
-})
 
 const User=mongoose.model('User',userSchema)
 export default User;

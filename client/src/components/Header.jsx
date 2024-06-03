38c64f02c63c658a315cea2d8df386fd46a7f589
Sudheesh A S <66734222+sudhi48@users.noreply.github.com>
2024-06-03 10:48:06 +0530

    complete home and about pages and changes in header(small bug fix)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   const {currentUser}=useSelector(state => state.user)
   return (
     <div className='bg-slate-200'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='font-semibold'>Auth App</h1>
             </Link>

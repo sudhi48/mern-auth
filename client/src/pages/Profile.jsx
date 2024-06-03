070d5aa87ffcf3a6357059d2908a48637512f0c4
Sudheesh A S <66734222+sudhi48@users.noreply.github.com>
2024-06-03 10:20:54 +0530

    add sign out user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOut,
   updateUserFailure,
   updateUserStart,
   updateUserSuccess
@@ -96,6 +97,15 @@ export default function Profile() {
       dispatch(deleteUserFailure(error))
     }
   }
+
+  const handleSignOut= async () => {
+    try {
+      await fetch('api/auth/signout');
+      dispatch(signOut())
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'> 
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -145,7 +155,7 @@ export default function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer' onClick={handleDeleteAccount}>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span className='text-red-700 cursor-pointer' onClick={handleSignOut}>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error && 'Something went wrong'}</p>
       <p className='text-green-700 mt-5'>{updateSuccess && 'User is updated successfully'}</p>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -45,8 +45,12 @@ const userSlice=createSlice({
         deleteUserFailure: (state,action)=>{
             state.error=action.payload;
             state.loading=false;
+        },
+        signOut: (state) =>{
+            state.currentUser=null;
+            state.loading=false;
+            state.error=false;
         }
-
     }        
 })
 
@@ -59,7 +63,8 @@ export const {
     updateUserFailure,
     deleteUserStart,
     deleteUserSuccess,
-    deleteUserFailure
+    deleteUserFailure,
+    signOut
 } = userSlice.actions;
 
 export default userSlice.reducer;
